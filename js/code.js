9b62af4e85b1f23c602eee3b706806b0dc2f7026
Dev-Nascimento <luizclaudio.rhi9@gmail.com>
2023-07-21 00:02:29 -0300

    Add Alterações Bag de responsividade no menu mobile ainda não resolvido

--- js/code.js
+++ js/code.js
@@ -7,6 +7,7 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
+
 /*****************************============= Scroll Sections Active links =============***************************/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -18,7 +19,7 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset &&  top < offset + height) {
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href *=' + id + ']').classList.add('active');
@@ -26,9 +27,9 @@ window.onscroll = () => {
         };
     });
 
-/*****************************============= Sticky NavBar =============***************************/
+    /*****************************============= Sticky NavBar =============***************************/
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
-/***********== Removendo toggle icon e navbar com click  navbar link (Scroll) ==******/
+    /***********== Removendo toggle icon e navbar com click  navbar link (Scroll) ==******/
 };
